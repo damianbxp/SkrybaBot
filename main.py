c71f9ecdcabf8881b7857e9de138d5362105c7b8
damianbxp <damianb.xp@gmail.com>
2022-02-10 15:46:20 +0100

    Restruct

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import random
-import skryba_bot as s
-from apikeys import *
-import json
 from discord.ext import commands
-
+from settings import *
 
 
 bot = commands.Bot(command_prefix="!")
 
 bot.load_extension(f'Commands.quoteCommand')
 
-bot.run(BOTTOKEN)
+bot.run(DISCORD_BOT_TOKEN)
